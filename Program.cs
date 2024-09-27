37932d25c3554c6b19e8fa72cc9372e4c9867629
Ndzalo <ndalomaluks@gmail.com>
2024-09-27 10:41:56 +0200

    updated

--- Program.cs
+++ Program.cs
@@ -7,30 +7,32 @@
            BeerBottles beerBottles = new BeerBottles();
             beerBottles.song();
         }
-        // Method to print a verse of the song based on the number of bottles
+        // Method to print a verse of the song 
         public void printVerse(int bottles)
         {
-            Console.WriteLine(bottles + " bottles of beer on the wall , " + bottles + " bottles of beer.");
-            Console.WriteLine("Take one down and pass it around, ");
-            Console.WriteLine();
+           
             // Check if there are more bottles left after taking one down
             if (bottles - 1 > 0)
             {
+                Console.WriteLine();
+                Console.WriteLine(bottles + " bottles of beer on the wall , " + bottles + " bottles of beer.");
+            Console.WriteLine("Take one down and pass it around, ");
                 bottles = bottles - 1;
-              
+
 
             }
-            else
+            if (bottles == 1)
             {
-                Console.WriteLine("No more bottles of beer on the wall, no more bottles of beer.");
+                Console.WriteLine();
+                Console.WriteLine(bottles + " bottle of beer on the wall , " + bottles + " bottle of beer.");
             }
         }
 
-        // Method to sing the entire song starting from 99 bottles
+        // Method to sing  
         public void song()
         {
-            // Loop from 99 down to 1 bottle
-            for ( int bottles = 99;  bottles > 0; bottles--)
+          
+            for ( int bottles = 99;  bottles > 1; bottles--)
             {
                 printVerse(bottles);
             }
