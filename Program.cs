4c410083337b13d3ad264f1862ecbe39a61ad0ce
Ndzalo <ndalomaluks@gmail.com>
2024-09-27 11:25:05 +0200

    updated

--- Program.cs
+++ Program.cs
@@ -3,26 +3,27 @@
     internal class BeerBottles
     {
         static void Main(string[] args)
-        {   
-           BeerBottles beerBottles = new BeerBottles();
+        {
+            BeerBottles beerBottles = new BeerBottles();
             beerBottles.song();
         }
         // Method to print a verse of the song 
         public void printVerse(int bottles)
         {
-           
+
             // Check if there are more bottles left after taking one down
-           
+
             if (bottles == 1)
             {
                 Console.WriteLine();
-                Console.WriteLine(bottles + " bottle of beer on the wall , " + bottles + " bottle of beer.");
+                Console.WriteLine(bottles + " bottle of beer on the wall , " + bottles + " bottle of beer." + " Take one down and pass it around ");
+                Console.WriteLine();
             }
             else
             {
                 Console.WriteLine();
-                Console.WriteLine(bottles + " bottles of beer on the wall , " + bottles + " bottles of beer.");
-                Console.WriteLine("Take one down and pass it around, ");
+                Console.WriteLine(bottles + " bottles of beer on the wall , " + bottles + " bottles of beer." + " Take one down and pass it around,");
+
                 bottles = bottles - 1;
 
 
@@ -32,12 +33,12 @@
         // Method to sing  
         public void song()
         {
-          
-            for ( int bottles = 99;  bottles > 0; bottles--)
+
+            for (int bottles = 99; bottles > 0; bottles--)
             {
                 printVerse(bottles);
             }
-           
+
         }
     }
 }
